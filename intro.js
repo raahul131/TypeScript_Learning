63cd0303e1bc87b935ae4658be4728a4c22f9f20
Rahul R Yadav <rryadav2025@gmail.com>
2023-09-15 16:13:47 +0545

    Initialization of the repo - Intro of TS

---
+++ intro.js
@@ -0,0 +1,6 @@
+var user = {
+    name: "rahul",
+    age: 10,
+};
+console.log("Rahul Ray Yadav");
+console.log(user.name);
---
+++ intro.ts
@@ -0,0 +1,7 @@
+let user = {
+  name: "rahul",
+  age: 10,
+};
+
+console.log("Rahul Ray Yadav");
+console.log(user.name);
